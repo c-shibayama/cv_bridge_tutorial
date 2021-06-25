7646f6ee8b0ba0e101fb01f3cd0e3b3aabbad2c6
shibayama <chifuyu.shibayama.r7@dc.tohoku.ac.jp>
2021-06-25 13:03:37 +0900

    syuusei

--- scripts/gazou.py
+++ scripts/gazou.py
@@ -1,31 +1,70 @@
 #!/usr/bin/env python
 import os
 import rospy
-from sensor_msgs.msg import Image
 import cv2
-from cv_bridge import CvBridge
+from std_msgs.msg import Float64MultiArray
+import numpy as np
 
 def operator():
-    rospy.init_node('operator', anonymous=True)
-    pub = rospy.Publisher('image_data', Image, queue_size=10)
+    #rospy.init_node('operator', anonymous=True)
+    #pub = rospy.Publisher('image_data', Image, queue_size=10)
 
     # read image
     #filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lena.png')
     #im = cv2.imread(filepath, cv2.IMREAD_COLOR)
     #cv2.imshow('image', im)
-    img = cv2.imread('lena.png')
-    cv2.imshow('image', img)
+    img = cv2.imread('src/cv_bridge_tutorial/scripts/ball.jpg')
+
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    circles = cv2.HoughCircles (img, cv2.HOUGH_GRADIENT, 1, 40, param1 = 500, param2 = 15, minRadius = 0, maxRadius = 0)
+    circles = np . uint16 ( np . around ( circles ) )
+    if circles is not None:
+        for i in circles [ 0 , : ] :
+	    cv2.circle ( img, (i[0], i[1]), i[2], (0, 255, 0), 2 )
+	    cv2.circle ( img, (i[0], i[1]), 2   , (0, 0, 255), 3 )
+	    print (i[0], i[1])
+            
 
     
+
+
+
+
+
+    pub = rospy.Publisher('circle', Float64MultiArray, queue_size=10)
+    rospy.init_node('gazou', anonymous=True)
+    r = rospy.Rate(10)
+
+
+    array = list(range(3))
+    array[0] = i[0]*0.01*0.1
+    array[1] = i[1]*0.01*0.1
+    array[2] = 0.3
+    posi = Float64MultiArray(data=array)
+    pub.publish(posi)
     
+
+
+    path = os.getcwd()
+    print(path)
+
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+
+ 
+
+
+
+
 
     # make bridge
-    bridge = CvBridge()
-    msg = bridge.cv2_to_imgmsg(im, encoding="bgr8")
-    rate = rospy.Rate(1) # 1hz
-    while not rospy.is_shutdown():
-        pub.publish(msg)
-        rate.sleep()
+    #bridge = CvBridge()
+    #msg = bridge.cv2_to_imgmsg(im, encoding="bgr8")
+    #rate = rospy.Rate(1) # 1hz
+    #while not rospy.is_shutdown():
+        #pub.publish(msg)
+        #rate.sleep()
 
 if __name__ == '__main__':
     try:
--- scripts/image_proc2.py
+++ scripts/image_proc2.py
@@ -6,6 +6,9 @@ from cv_bridge import CvBridge
 import cv2
 import numpy as np
 
+l = []
+j = []
+
 def process_image(msg):
     try:
         bridge = CvBridge()
@@ -18,18 +21,34 @@ def process_image(msg):
 	#cv2.circle(img, (240, 35), 20, (0, 0, 0), thickness=3, lineType=cv2.LINE_AA)
 	circles = cv2.HoughCircles (img, cv2.HOUGH_GRADIENT, 1, 20, param1 = 500, param2 = 30, minRadius = 0, maxRadius = 0)
 	circles = np . uint16 ( np . around ( circles ) )
-	for i in circles [ 0 , : ] :
-	    cv2 . circle ( orig , ( i [ 0 ] , i [ 1 ] ) , i [ 2 ] , ( 0 , 255 , 0 ) , 2 )
-	    cv2 . circle ( orig , ( i [ 0 ] , i [ 1 ] ) , 2 , ( 0 , 0 , 255 ) , 3 )
-	    #print (i[0], i[1])
 
-	print (i[0], i[1])
+	
+	
+	#a = []
+	for i in circles [ 0 , : ] :
+	    cv2.circle ( orig, (i[0], i[1]), i[2], (0, 255, 0), 2 )
+	    cv2.circle ( orig, (i[0], i[1]), 2   , (0, 0, 255), 3 )
+	    #a.append(i)
+	    l.append(i[0])
+	    j.append(i[1])
+	    #print(l)
+	    
+	    #print(j)
+            #a = i[0] 
+
+            #print (l[0], j[0])
+            #print (l[4], j[4])
+	   
+            
+        #print(circles [0,0])
+
 	imgMsg = bridge.cv2_to_imgmsg(img, "mono8")
         pub = rospy.Publisher('image_gray', Image, queue_size=10)
         pub.publish(imgMsg)
 
 	cv2.imshow('image', orig)
-        cv2.waitKey(10)
+        cv2.waitKey(1)
+ 
     except Exception as err:
         print err
 
